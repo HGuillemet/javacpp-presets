0e527af241e4523e7a45568d30207b8fa2390e1d
Hervé Guillemet <hg@apteryx.fr>
2024-08-02 15:29:30 +0200

    Use chrono from javacpp

--- pytorch/src/gen/java/org/bytedeco/pytorch/WorkInfo.java
+++ pytorch/src/gen/java/org/bytedeco/pytorch/WorkInfo.java
@@ -2,12 +2,20 @@
 
 package org.bytedeco.pytorch;
 
-import org.bytedeco.pytorch.chrono.*;
+import org.bytedeco.pytorch.Allocator;
+import org.bytedeco.pytorch.Function;
+import org.bytedeco.pytorch.Module;
 import org.bytedeco.javacpp.annotation.Cast;
+import org.bytedeco.pytorch.helper.*;
+import java.nio.*;
 import org.bytedeco.javacpp.*;
 import org.bytedeco.javacpp.annotation.*;
 
+import static org.bytedeco.javacpp.presets.javacpp.*;
+import static org.bytedeco.openblas.global.openblas_nolapack.*;
 import static org.bytedeco.openblas.global.openblas.*;
+import org.bytedeco.javacpp.chrono.*;
+import static org.bytedeco.javacpp.chrono.Chrono.*;
 
 import static org.bytedeco.pytorch.global.torch.*;
 
